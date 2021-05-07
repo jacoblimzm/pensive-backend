f8f318de234c2e6998e03e9a007f82fe63f81da9
jacoblimzm <jacob_lim@live.com>
2021-05-07 22:36:53 +0800

    add sign up view

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
          name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
+    path("user/login/", views.LoginView.as_view(), name="user-login"),
+    
 ]
