efc043c368ecd7d2b4d43d1fda51e4b7a2f2c784
jacoblimzm <jacob_lim@live.com>
2021-05-07 21:46:08 +0800

    remove print

--- pensive/settings.py
+++ pensive/settings.py
@@ -27,10 +27,6 @@ load_dotenv()
 # SECRET_KEY = load_dotenv("SECRET_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-print( f"line 30 in settings {SECRET_KEY}")
-# SECURITY WARNING: don't run with debug turned on in production!
-
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
