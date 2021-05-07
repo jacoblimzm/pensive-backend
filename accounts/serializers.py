b2fafc95179302b23a14f66d0cd9cbedebcff246
jacoblimzm <jacob_lim@live.com>
2021-05-07 21:46:38 +0800

    add serializers for User and Token

---
+++ accounts/serializers.py
@@ -0,0 +1,15 @@
+# accounts.serializers.py
+# 
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+# the fields included here are the data from the models you want to send back as Json data, through the views.py
+# they don't have to include EVERY field. can exclude as you wish. 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+class TokenSerializer(serializers.Serializer):
+    token = serializers.CharField(max_length=255)
