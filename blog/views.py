b3f2bcc74fce2845afdc106adcb97a380bc46399
jacoblimzm <jacob_lim@live.com>
2021-05-09 21:54:58 +0800

    add blog edit view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework import permissions, generics, status
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 from .models import BlogEntry, Category
 from .serializers import BlogEntrySerializer, CategorySerializer
 
 from django.contrib.auth.models import User # the default User model from django
 from django.contrib.auth import authenticate, login
+
 
 
 class CategoryListView(generics.ListAPIView):
@@ -94,4 +96,68 @@ class BlogEntryCreateView(generics.ListCreateAPIView):
     
     
     
+    ## Create Blog Edit/Update/Delete View
+    
+
+class BlogEntryEditView(generics.RetrieveUpdateDestroyAPIView):
+    
+    queryset = BlogEntry.objects.order_by("-created_on")
+    serializer_class = BlogEntrySerializer
+    lookup_field = "slug"
+    permission_classes = (permissions.AllowAny,)
+    
+    def put(self, request, *args, **kwargs):
+        # "slug" and "created_at" not necessary since those will be automatically generated on save
+        entry_id = request.data.get("id")
+        title = request.data.get("title")
+        image = request.data.get("image")
+        excerpt = request.data.get("excerpt")
+        month = request.data.get("month") # should be a dropdown in frontend with THREE LETTER MONTHS
+        day = request.data.get("day") # should be a dropdown in frontend with DIGIT DAYS
+        content = request.data.get("content") 
+        breaking = request.data.get("breaking") # boolean. drop down in FRONTEND
+        category = request.data.get("category") # should be a dropdown in frontend with uncapped category names
+        print(entry_id)
+        # print(title, image, excerpt, month, day, content, breaking, category)
+        if not request.data:
+            return Response(data={
+                    "success": False,
+                    "message": "no data received"
+                    }
+            )
+        elif not title or not image or not excerpt or not month or not day or not content or not category: # cannot include "breaking" here as it will mess up the conditional
+            return Response(data={
+                    "success": False,
+                    "message": "ensure all fields are filled in!"
+                }
+            )
+        
+        else:
+            existing_post = BlogEntry.objects.get(pk=entry_id)
+            category_result = Category.objects.get(category_name__iexact = category) # search database to get back categories and their respective id
+            category_id = category_result.id # with the new updated category, need to find the id and assign it accordingly
+            
+            existing_post.title = title
+            existing_post.image = image
+            existing_post.excerpt = excerpt
+            existing_post.month = month
+            existing_post.day = day
+            existing_post.content = content
+            existing_post.breaking = breaking
+            existing_post.category_id = category_id # the django model automatically assigns and _id to a foreign key. remember to include
+            
+            
+            # print(existing_post.category_id) 
+            existing_post.save()
+            
+            
+            return Response(data={
+                "success": True,
+                "message": f"'{title}' has been updated!"
+            })
+            
+            
+            
+            # return JsonResponse(existing_post.values("title"), safe=False)
+        
     
