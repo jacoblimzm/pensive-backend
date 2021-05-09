5033ebf893b9097844dda905c7c55fec7afda3ab
jacoblimzm <jacob_lim@live.com>
2021-05-09 09:46:40 +0800

    add static folder imports and urls

--- pensive/urls.py
+++ pensive/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")), # for login and register
     path("api-auth/", include("rest_framework.urls")), # for the django restframework
     path('summernote/', include('django_summernote.urls')), # for summernote capabilities
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # summernote settings
+
+
