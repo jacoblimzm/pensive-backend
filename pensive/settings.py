27c45a9d8ad20141c98a3641deae7933971bd3ae
jacoblimzm <jacob_lim@live.com>
2021-05-09 09:45:02 +0800

    add django summernote and blog to apps

--- pensive/settings.py
+++ pensive/settings.py
@@ -45,8 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "corsheaders",
-    "django_summernote"
+    "django_summernote",
     "accounts",
+    "blog",
 ]
 
 MIDDLEWARE = [
@@ -129,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -184,5 +185,5 @@ SUMMERNOTE_THEME = "bs4"
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000", # allow connections from front end CRA
+    "http://localhost:3000", # allow connections from front end CRA
 ]
