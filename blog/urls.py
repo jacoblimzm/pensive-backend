779d7242c19ee01b52b3e69c560e2bf8978d0e97
jacoblimzm <jacob_lim@live.com>
2021-05-09 15:44:31 +0800

    add urls for categories and blog post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogEntryListView, CategoryListView
+from .views import BlogEntryListView, BlogEntryDetailView, CategoryListView, CategoryDetailView
 
 urlpatterns = [
     path("", BlogEntryListView.as_view()),
-    
+    path("categories", CategoryListView.as_view()),
+    path("categories/<category_name>", CategoryDetailView.as_view()),
+    path("<slug>", BlogEntryDetailView.as_view()),
 ]
