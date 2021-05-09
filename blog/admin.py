a6cb9ab49a6bc51e8dd3257b5a1e2bbde15c1635
jacoblimzm <jacob_lim@live.com>
2021-05-09 09:58:57 +0800

    add blog entry and category model to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import BlogEntry, Category
 # Register your models here.
+# if you don't you won't be able to add/edit/delete your models in the admin panel
+from django_summernote.admin import SummernoteModelAdmin
+
+
+class BlogEntryAdmin(SummernoteModelAdmin):
+    exclude = ("slug",)
+    list_display = ("id", "slug", "title", "category", "created_on", "breaking")
+    list_display_links = ("id", "title")
+    search_fields = ("title", )
+    list_per_page = 20
+    summernote_fields = ('content',) # specifically targeting the "content" TextField() in BlogEntry model
+
+admin.site.register(BlogEntry, BlogEntryAdmin)
+admin.site.register(Category)
+
+
