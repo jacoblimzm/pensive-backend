434c9d7438c588af014b8cd09d6cb11fb02e10bb
jacoblimzm <jacob_lim@live.com>
2021-05-09 09:58:05 +0800

    add serialisers for blogentry and category

---
+++ blog/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import BlogEntry, Category
+
+
+class BlogEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogEntry
+        fields = "__all__"
+        lookup_field = "slug" # this tells the serializer to query the database using the "slug" property instead of "id" when passed a param in the urls.py
+        
+        
+class CategorySerialiser(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            "category_name",
+            "blogentries", # recall that one category can have many entries, so this is how they are linked
+        )
+        depth = 1        
