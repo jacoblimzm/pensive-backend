9ca1de48f9c6163e756b483fed55f842b323afa4
jacoblimzm <jacob_lim@live.com>
2021-05-13 18:10:59 +0800

    add urls for chatroom template

---
+++ chat/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views # import the FUNCTIONS from views.py in the same level folder. which serve up the html files
+
+# path("",) here will BUILD on the BASE URL defined in urls.py in project folder
+# think of this as the tunr.get(), tunr.post().
+
+# what is IMPORTANT here is that what you assign to "name" will be what you use in <a> tags in HTML to link to other HTMLs
+# this is to create a 'once source of truth' for your paths, in case the path is used many times across the app
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<str:room_name>/", views.chat_room, name="chat_room")
+]
