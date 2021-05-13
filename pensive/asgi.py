02994e0a5da2e597a821f386a91a37513be7ccf5
jacoblimzm <jacob_lim@live.com>
2021-05-13 23:32:26 +0800

    import django to setup settings for daphne

--- pensive/asgi.py
+++ pensive/asgi.py
@@ -10,16 +10,17 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 # similar to how "urls" in the project folder imports urls from various apps that extend the paths,
 # "routing" in the project folder will import routes from varoius apps which have their own "routing" files
 # that will extend the BASE path of routing
-
+import django
+django.setup() # DJANGO_SETTINGS_MODULE must be defined in environement variables
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing # importing from chat folder
-
 import os
-
 from django.core.asgi import get_asgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pensive.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
